1c4bf54e736e88428bcffa978ed5453fc6e3df39
하성욱[쇼핑서비스FE] <seongwook.ha@navercorp.com>
2022-04-26 14:36:15 +0900

    check nonexistant dependency

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const core = require('@actions/core')
 const parser = require('action-input-parser')
 const fs = require('fs')
 
+core.setCommandEcho(true)
+
 try {
   const dirA = core.getInput('dirA')
   const dirB = core.getInput('dirB')
@@ -15,6 +17,13 @@ try {
   const packages = parser.getInput('packages', { type: 'array' }) || []
 
   packages.forEach((package) => {
+    if (
+      !dirAConfig.dependencies[package] ||
+      !dirBConfig.dependencies[package]
+    ) {
+      core.info('package does not exist in one of the dependencies' + package)
+      return
+    }
     const packageA = dirAConfig.dependencies[package]
     const packageB = dirBConfig.dependencies[package]
 
